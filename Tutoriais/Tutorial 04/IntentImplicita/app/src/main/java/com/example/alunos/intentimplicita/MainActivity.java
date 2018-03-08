b01abb09432ec55e974497093d39e53414235e98
Pedro <pedrolucascefet@gmail.com>
2018-03-08 11:18:39 -0300

    Intent Implicita

---
+++ Tutoriais/Tutorial 04/IntentImplicita/app/src/main/java/com/example/alunos/intentimplicita/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.alunos.intentimplicita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openURL(View v){
+        EditText url =
+    }
+}
